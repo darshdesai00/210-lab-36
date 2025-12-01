d6ebae91104687df17278dfad146cffcda9e61e5
darshdesai00 <darshdesai50@icloud.com>
2025-11-30 21:21:40 -0800

    Working on Milestone 4

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 #include <string>
 using namespace std;
 
+void showMenu() {
+    cout << "\n===== MENU =====\n";
+    cout << "1. Add record\n";
+    cout << "2. Delete record\n";
+    cout << "3. Search for record\n";
+    cout << "4. Modify record\n";
+    cout << "5. Display records (inorder)\n";
+    cout << "6. Quit\n";
+    cout << "Enter option: ";
+}
+
 int main() {
 
     IntBinaryTree tree;
@@ -14,20 +25,44 @@ int main() {
         return 1;
     }
 
-    cout << "Loading record from the codes.txt file...\n";
-
     string code;
 
+    cout << "Loading records from codes.txt...\n";
     while (infile >> code) {
         tree.insertNode(code);
     }
 
     infile.close();
-    cout << "All records have been loaded into the BST.\n\n";
+    cout << "All records have been loaded.\n";
     
-    cout << "\nInorder traversal (alphabetical):\n";
-    tree.displayInOrder();
+    int choice;
+    string oldValue, newValue;
 
+    do {
+        showMenu();
+        cin >> choice;
+
+        switch (choice) {
+
+        case 1:
+            cout << "Enter code to add: ";
+            cin >> code;
+            tree.insertNode(code);
+            cout << code << " added.\n";
+            break;
+        
+        case 2:
+            cout << "Enter code to delete: ";
+            cin >> code;
+            tree.remove(code);
+            cout << code << " deleted (if it existed).\n";
+            break;
+        
+        
+        
+        
+        }
+    }
     return 0;
 }
 
